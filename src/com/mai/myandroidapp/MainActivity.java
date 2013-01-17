b24045bb161d26d919f0e901b42b74ecc701c716
yangwenmai <yangwen.yw@gmail.com>
2013-01-17 11:28:15 +0800

    add sendMessage method

--- src/com/mai/myandroidapp/MainActivity.java
+++ src/com/mai/myandroidapp/MainActivity.java
@@ -1,8 +1,9 @@
 package com.mai.myandroidapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void sendMessage(View view) {
+		
 	}
 
 }
