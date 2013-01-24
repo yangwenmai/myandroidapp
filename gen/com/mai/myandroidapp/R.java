a3785b74728250ede27b59ab85298c40c09b124d
yangwenmai <yangwen.yw@gmail.com>
2013-01-24 09:32:30 +0800

    test

--- gen/com/mai/myandroidapp/R.java
+++ gen/com/mai/myandroidapp/R.java
@@ -8,33 +8,70 @@
 package com.mai.myandroidapp;
 
 public final class R {
+    public static final class array {
+        public static final int shaheshang=0x7f050003;
+        public static final int sunwukong=0x7f050001;
+        public static final int tangseng=0x7f050000;
+        public static final int zhubajie=0x7f050002;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shaheshang=0x7f020001;
+        public static final int sunwukong=0x7f020002;
+        public static final int tangseng=0x7f020003;
+        public static final int zhubajie=0x7f020004;
     }
     public static final class id {
-        public static final int button=0x7f070001;
-        public static final int listView=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int text=0x7f070000;
+        public static final int button=0x7f080002;
+        public static final int imageView1=0x7f080005;
+        public static final int item_imageView=0x7f080003;
+        public static final int item_textView=0x7f080004;
+        public static final int linearLayout1=0x7f080009;
+        public static final int listView1=0x7f08000b;
+        public static final int listView2=0x7f08000d;
+        public static final int listView3=0x7f08000f;
+        public static final int listView4=0x7f080011;
+        public static final int menu_settings=0x7f080016;
+        public static final int notify_cancal=0x7f080015;
+        public static final int pass=0x7f080008;
+        public static final int shaheshang=0x7f080014;
+        public static final int sunwukong=0x7f080012;
+        public static final int tab1=0x7f08000a;
+        public static final int tab2=0x7f08000c;
+        public static final int tab3=0x7f08000e;
+        public static final int tab4=0x7f080010;
+        public static final int text=0x7f080001;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080007;
+        public static final int user=0x7f080006;
+        public static final int zhubajie=0x7f080013;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int gallery_item=0x7f030001;
+        public static final int grid_item=0x7f030002;
+        public static final int image=0x7f030003;
+        public static final int login=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int main1=0x7f030006;
+        public static final int main2=0x7f030007;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int about=0x7f040004;
+        public static final int about=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int button=0x7f040003;
+        public static final int button=0x7f040004;
         public static final int hello=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int shaheshang=0x7f040008;
-        public static final int sunwukong=0x7f040006;
-        public static final int tangseng=0x7f040005;
-        public static final int zhubajie=0x7f040007;
+        public static final int menu_settings=0x7f040003;
+        public static final int msg=0x7f040002;
+        public static final int shaheshang=0x7f040009;
+        public static final int sunwukong=0x7f040007;
+        public static final int tangseng=0x7f040006;
+        public static final int zhubajie=0x7f040008;
     }
     public static final class style {
         /** 
@@ -58,10 +95,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
